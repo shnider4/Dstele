3ae98fca1f1f96a4efaf8b35ce240c2dcc950519
matrix <64998320+shnider0@users.noreply.github.com>
2021-07-25 08:57:30 +0300

    Update black.py

--- functions/black.py
+++ functions/black.py
@@ -36,9 +36,9 @@ async def amere_B(client, message):
                 im.save(edit_img_loc, quality=100)
                 y_text += height
             y_bot = (h / 1.8) + (height / 1.8 * len(lines))
-            y_top = (y_text / 1.8 - height) / (len(lines))
+            y_top = (y_text / 1.5 - height) / (len(lines))
             font = ImageFont.truetype(dir_font, 45)
-            draw.multiline_text((((w - width) /2.8), y_text + (height / 4)), "qad3im", fill=(0, 0, 0), font=font,
+            draw.multiline_text((((w - width) /2.8), y_text + 30)), "qad3im", fill=(0, 0, 0), font=font,
                                 align="right")
             await ms.edit("يتم التحميل ...")
             im.save(edit_img_loc, quality=100)
