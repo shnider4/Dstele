271c18aed147e3157a232375a02a7d6033f211db
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-16 14:18:47 +0300

    Update edit2.py

--- functions/edit2.py
+++ functions/edit2.py
@@ -4,7 +4,7 @@ async def amiri_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/Amiri.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -13,7 +13,7 @@ async def amiri_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/Amiri.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -21,7 +21,7 @@ async def rqaa_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/rqaa.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -29,7 +29,7 @@ async def rqaa_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/rqaa.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -37,7 +37,7 @@ async def rqaa2_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/rqaa2.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 95
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -45,7 +45,7 @@ async def rqaa2_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/rqaa2.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 95
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -53,7 +53,7 @@ async def qran_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qraan.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -61,7 +61,7 @@ async def qran_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qraan.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -69,7 +69,7 @@ async def tbaa_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/tbaa.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 80
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -77,7 +77,7 @@ async def tbaa_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/tbaa.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 80
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -85,7 +85,7 @@ async def hsha_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hsha.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -93,7 +93,7 @@ async def hsha_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hsha.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -101,7 +101,7 @@ async def qyass_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qyasee.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -109,7 +109,7 @@ async def qyass_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qyasee.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 90
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -117,7 +117,7 @@ async def qyass2_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qyasee2.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -125,7 +125,7 @@ async def qyass2_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/qyasee2.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -133,7 +133,7 @@ async def hur_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hur.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -141,7 +141,7 @@ async def hur_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hur.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -149,7 +149,7 @@ async def hur2_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hur2.ttf"
     color_t = (0, 0, 0)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -157,7 +157,7 @@ async def hur2_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/hur2.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 100
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -165,7 +165,7 @@ async def alanat_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/alanat.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -173,7 +173,7 @@ async def alanat_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/alanat.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -181,7 +181,7 @@ async def alanat2_BXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/alanat2.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
 
 
@@ -189,5 +189,5 @@ async def alanat2_WXX(client, message):
     dir_bg = "./bg2"
     dir_font = "./fonts/alanat2.ttf"
     color_t = (250, 250, 250)
-    size_font = 105
+    size_font = 70
     await edit1(client, dir_bg, dir_font, color_t, size_font, message)
