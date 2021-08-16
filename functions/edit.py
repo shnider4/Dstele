2eda045fc5f73475673cdc57c36a45df7bc35072
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-16 14:31:55 +0300

    Update edit.py

--- functions/edit.py
+++ functions/edit.py
@@ -256,7 +256,7 @@ async def alanat2_B(client, message):
 
 async def alanat2_W(client, message):
     dir_bg = "./bg"
-    dir_font = "./fonts/alanat2.ttf"
+    dir_font = "./fonts/add.ttf"
     color_t = (250, 250, 250)
     size_font = 70
     codx = "alanat2_WX"
